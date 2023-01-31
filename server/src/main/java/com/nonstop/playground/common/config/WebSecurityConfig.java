e3c543ebf7fd04c7a2c208bb64e479f606cbe7e4
Bochan kim <44803830+bo1030@users.noreply.github.com>
2023-02-01 01:01:42 +0900

    feat: 스프링 security 컨피그 최초 커밋

---
+++ server/src/main/java/com/nonstop/playground/common/config/WebSecurityConfig.java
@@ -0,0 +1,9 @@
+package com.nonstop.playground.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+}
