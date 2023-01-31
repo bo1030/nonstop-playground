a600b5fb07adf97bc1288d45ea574e7a97e23649
Bochan kim <44803830+bo1030@users.noreply.github.com>
2023-01-31 23:57:18 +0900

    feat: 유저 어플리케이션 서비스와 컨트롤러 추가

---
+++ server/src/main/java/com/nonstop/playground/user/application/UserService.java
@@ -0,0 +1,13 @@
+package com.nonstop.playground.user.application;
+
+import com.nonstop.playground.user.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+}
