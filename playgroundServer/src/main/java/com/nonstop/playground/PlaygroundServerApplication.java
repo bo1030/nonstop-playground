aaef2ccc7d82c91d33ee13c1e624dfbd6bc58816
LGUPLUS-2022KTD\User <qhcks1030@naver.com>
2023-01-31 14:33:24 +0900

    feat: 백엔드 최초 커밋

---
+++ playgroundServer/src/main/java/com/nonstop/playground/PlaygroundServerApplication.java
@@ -0,0 +1,13 @@
+package com.nonstop.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundServerApplication.class, args);
+	}
+
+}
---
+++ playgroundServer/src/test/java/com/nonstop/playground/PlaygroundServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonstop.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
