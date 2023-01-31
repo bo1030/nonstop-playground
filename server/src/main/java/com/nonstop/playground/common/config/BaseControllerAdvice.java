8eddac8f8f5e2b7ea0ebe3b0cce3da385bf88e85
Bochan kim <44803830+bo1030@users.noreply.github.com>
2023-01-31 23:13:24 +0900

    feat: validationError 핸들러 추가

--- server/src/main/java/com/nonstop/playground/common/config/BaseControllerAdvice.java
+++ server/src/main/java/com/nonstop/playground/common/config/BaseControllerAdvice.java
@@ -3,15 +3,25 @@ package com.nonstop.playground.common.config;
 import com.nonstop.playground.common.exception.BaseException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
 public class BaseControllerAdvice {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(e.getBindingResult().getAllErrors().stream().collect(Collectors.toMap(o -> ((FieldError) o).getField(), o -> o.getDefaultMessage())));
+    }
+
     @ExceptionHandler(BaseException.class)
-    public <T extends  BaseException> ResponseEntity<BaseException> handleBaseExecption(T customException) {
+    public <T extends BaseException> ResponseEntity<BaseException> handleBaseExecption(T customException) {
         log.error(customException.getClassName() + "is occurred!", customException);
 
         return ResponseEntity.internalServerError().body(customException);
