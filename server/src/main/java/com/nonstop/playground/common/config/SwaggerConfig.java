316399cf34474307f3c490e8ab3a660164caf326
Bochan kim <44803830+bo1030@users.noreply.github.com>
2023-01-31 23:36:44 +0900

    feat: 스웨거 설정 user 그룹 추가

--- server/src/main/java/com/nonstop/playground/common/config/SwaggerConfig.java
+++ server/src/main/java/com/nonstop/playground/common/config/SwaggerConfig.java
@@ -1,24 +1,28 @@
 package com.nonstop.playground.common.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(title = "논스톱 플레이그라운드 API",
-                description = "논스톱 보드게임 동아리 플레이그라운드 API 명세서",
-                version = "v1"))
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public GroupedOpenApi chatOpenApi() {
-        String[] paths = {"api/v1/**"};
-
+    public GroupedOpenApi userAPI() {
         return GroupedOpenApi.builder()
-                .group("논스톱 플레이그라운드 API v1")
-                .pathsToMatch(paths)
+                .group("users")
+                .pathsToMatch("/users/**")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("논스톱 플레이그라운드 프로젝트")
+                        .description("논스톱 보드게임 동아리 플레이그라운드 프로젝트 입니다.")
+                        .version("v0.0.1"));
     }
 }
