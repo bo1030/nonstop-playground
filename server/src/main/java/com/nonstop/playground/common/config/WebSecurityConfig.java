78bed091815a0291f53326c27ffab38eb90ab4a5
Bochan Kim <qhcks1030@naver.com>
2023-02-01 13:41:29 +0900

    feat: WebSecurityConfig 임시 설정 추가

--- server/src/main/java/com/nonstop/playground/common/config/WebSecurityConfig.java
+++ server/src/main/java/com/nonstop/playground/common/config/WebSecurityConfig.java
@@ -1,9 +1,36 @@
 package com.nonstop.playground.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests()
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .disable()
+                .csrf()
+                .disable()
+                .headers()
+                .disable()
+                .httpBasic()
+                .disable()
+                .rememberMe()
+                .disable()
+                .logout()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return httpSecurity.build();
+    }
 }
